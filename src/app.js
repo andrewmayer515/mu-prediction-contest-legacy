46e8b4f15ca9b5c0ad01cda9c5bc840e3362fb81
Andrew Mayer <andrewmayer515@gmail.com>
2018-07-22 19:03:28 -0500

    get username for posts, format data

--- src/app.js
+++ src/app.js
@@ -1,20 +1,14 @@
 const puppeteer = require('puppeteer');
-const ProgressBar = require('progress');
+// const ProgressBar = require('progress');
 const auth = require('../config/auth.json');
 const results = require('../config/results.json');
 
 const args = process.argv.slice(2);
 const isDebug = args.includes('--debug');
 const launchSettings = isDebug ? { headless: false } : {};
-const bar = new ProgressBar(':token [:bar] :elapsed', {
-  total: 10,
-  incomplete: ' ',
-});
+const data = [];
 
 (async () => {
-  bar.tick({
-    token: 'Logging in...',
-  });
   const browser = await puppeteer.launch(launchSettings);
   const page = await browser.newPage();
   if (isDebug) { // Forces the browser view to fill the viewport size while running as debug
@@ -30,22 +24,25 @@ const bar = new ProgressBar(':token [:bar] :elapsed', {
 
   await page.click('#guest_form > input.button_submit');
   await page.waitForNavigation();
-  await bar.tick({
-    token: 'Gathering predictions...',
-  });
 
   // Total length of posts on a page
-  const elements = await page.$$('#quickModForm > div');
+  // const elements = await page.$$('#quickModForm > div');
   // console.log(elements.length);
 
-  const textArray = await page.evaluate(() => [...document.querySelectorAll('.post > .inner')].map(elem => elem.innerText)); // eslint-disable-line no-undef
-  // console.log(textArray);
+  const usernameArray = await page.evaluate(() => [...document.querySelectorAll('.poster > h4')].map(elem => elem.innerText)); // eslint-disable-line no-undef
+  const commentArray = await page.evaluate(() => [...document.querySelectorAll('.post > .inner')].map(elem => elem.innerText)); // eslint-disable-line no-undef
+
+  // combine fields to get prediction data
+  await usernameArray.forEach((value, index) => {
+    data.push({
+      username: usernameArray[index],
+      comment: commentArray[index].split('\n'),
+    });
+  });
+
+  console.log(data);
 
   if (!isDebug) { // Keep browser open while running as debug
     await browser.close();
   }
-
-  await bar.tick({
-    token: 'Done!',
-  });
 })();
